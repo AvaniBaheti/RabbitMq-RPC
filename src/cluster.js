dcd0029bcc4955df20e285fd1912d2f83ce6bea7
Avani Baheti <avani@digitalgreen.org>
2025-09-17 12:41:46 +0530

    feat: enable multi-core cluster mode

--- src/cluster.js
+++ src/cluster.js
@@ -1,11 +1,29 @@
+import cluster from 'node:cluster';
+import os from 'node:os';
 import { initRpcClient } from './rpcClient.js';
 import { startReplyRouter } from './replyRouter.js';
 import { startHttpServer } from './httpServer.js';
 
-(async () => {
-  const workerId = 1; 
-  await initRpcClient();
-  await startReplyRouter();
-  await startHttpServer(workerId);
-  console.log(`[worker ${workerId}] up`);
-})();
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+  console.log(`[master] starting ${numCPUs} HTTP workers`);
+  for (let i = 0; i < numCPUs; i++) cluster.fork();
+
+  cluster.on('exit', (worker) => {
+    console.log(`[master] worker ${worker.id} died. Spawning replacement...`);
+    cluster.fork();
+  });
+} else {
+  const workerId = cluster.worker.id;
+
+  (async () => {
+    await initRpcClient();
+    await startReplyRouter();
+    await startHttpServer(workerId);
+    console.log(`[worker ${workerId}] up`);
+  })().catch(err => {
+    console.error(`[worker ${workerId}] fatal`, err);
+    process.exit(1);
+  });
+}
